c86d056d8c342bbbc515024f2f421c1eed7fe895
Edgard Morales <edgardmv04@gmail.com>
2022-06-23 10:00:00 -0600

    add timestamps

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,16 +5,19 @@ interface IUser {
   password: string;
 }
 
-const userSchema = new Schema<IUser>({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default model<IUser>("User", userSchema);
