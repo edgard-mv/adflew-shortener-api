211f47c0391a485b252a9773d49ca259fd3c9b51
Edgard Morales <edgardmv04@gmail.com>
2022-06-24 00:26:56 -0600

    handle url info retrieval and short code generation

---
+++ src/controllers/link.controller.ts
@@ -0,0 +1,67 @@
+import HttpStatusCodes from "http-status-codes";
+
+// Types
+import { Request, Response } from "express";
+
+import Link from "models/Link";
+import { validationResult } from "express-validator";
+import { getNewIdForUrl } from "utils/urls";
+import { User } from "models";
+
+// Return corresponding URL, given shortCode
+export const getUrlFromShortCode = async (req: Request, res: Response) => {
+  const { shortCode } = req.params;
+  const link = await Link.findOne({ shortCode });
+
+  if (!link) {
+    return res.status(HttpStatusCodes.NOT_FOUND).send("URL not found");
+  }
+
+  await link.populate("user", ["id", "username"]);
+
+  return res.json({ link });
+};
+
+// Return new, or retrieved, url-shortCode pair
+export async function createShortCode(req: Request, res: Response) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(HttpStatusCodes.BAD_REQUEST)
+      .json({ errors: errors.array() });
+  }
+
+  const user = await User.findById(req.userId).select("-password");
+
+  if (!user) {
+    return res.status(HttpStatusCodes.NOT_FOUND).json({
+      errors: [
+        {
+          msg: "User not found",
+        },
+      ],
+    });
+  }
+
+  const { url } = req.body;
+
+  // verify url isn't registered already by this user
+  const existingLink = await Link.findOne({ url, user: user.id }).populate(
+    "user",
+    ["username", "id"]
+  );
+  if (existingLink) {
+    return res.json({ link: existingLink });
+  }
+
+  // register new link
+  let shortCode = await getNewIdForUrl();
+  const link = new Link({ url, user: req.userId, shortCode });
+  user.links.push(link.id);
+
+  Promise.allSettled([user.save(), link.save()]);
+
+  await link.populate("user", ["username", "id"]);
+
+  return res.json({ link });
+}
--- src/routes/links.ts
+++ src/routes/links.ts
@@ -1,9 +1,42 @@
-import express from "express";
+import { Router } from "express";
 
-const linksRouter = express.Router();
+import { body, param } from "express-validator";
 
-linksRouter.get("/", (_, res) => {
-  res.status(200).send({ data: { message: "routes setup" } });
-});
+// handlers
+import {
+  createShortCode,
+  getUrlFromShortCode,
+} from "controllers/link.controller";
+import { isValidHttpUrlWithTld } from "utils/urls";
+import authMiddleware from "middleware/auth.middleware";
+
+const linksRouter = Router();
+
+// @route   GET api/links/:shortCode
+// @desc    Retrieve url info given corresponding shortCode
+// @access  Public
+linksRouter.get(
+  "/:shortCode",
+  param("shortCode", "Invalid short code").isLength({ min: 7 }),
+  getUrlFromShortCode
+);
+
+// @route   POST api/links
+// @desc    Register url-shortCode pair given url
+// @access  Private
+linksRouter.post(
+  "/",
+  [
+    authMiddleware,
+    body("url").custom((value) => {
+      const result = isValidHttpUrlWithTld(value);
+      if (!result.isValid) {
+        throw new Error(result.reason);
+      }
+      return result.isValid;
+    }),
+  ],
+  createShortCode
+);
 
 export default linksRouter;
